c92c5ec6c59e80e3cfe7e77d4608fe70bcea60c5
Aldo Leka <aldo.leka@live.com>
2025-09-15 08:51:32 +0200

    remove commented lines from stats.py

--- stats.py
+++ stats.py
@@ -25,36 +25,3 @@ def sort_usages(usages: dict) -> list:
 def sort_on(usages):
     return usages["num"]
 
-
-# def get_chars_usage_(text: str) -> list:
-#     usages = []
-#     for ch in text:
-#         usage = get_usage(usages, ch)
-#         if usages is None:
-#             set_usage(usages, ch, 0)
-#         else:
-#             set_usage(usages, ch, usage + 1)
-    
-#     return usages
-
-
-# def get_usage(usages: list, char: str) -> int | None:
-#     for dict in usages:
-#         for value in dict:
-#             if char == value["char"]:
-#                 return value["usage"]
-    
-#     return None
-
-
-# def set_usage(usages: list, char: str, usage: int):
-#     found = False
-#     for dict in usages:
-#         for value in dict:
-#             if char == value["char"]:
-#                 found = True
-#                 value["usage"] = usage
-#                 break
-    
-#     if not found:
-#         usages.append({"char": char, "usage": usage})
